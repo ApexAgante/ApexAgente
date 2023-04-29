996c376007bee25d43f677cee806bf27c0b956d9
Jo Project <122333339+jo-project@users.noreply.github.com>
2023-04-30 00:59:35 +0700

    Delete app directory

--- app/controller.py
+++
@@ -1,9 +0,0 @@
-def check_user(user: str, password: str):
-    if not user:
-        return None
-    if user.strip() == 'admin':
-        if password == '12345':
-            return 'Administrator'
-        else:
-            return None
-    return user.capitalize()
--- app/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from gui.login.gui import login_window
-
-root = tk.Tk()
-root.withdraw()
-
-if __name__ == "__main__":
-    login_window(root)
-    root.mainloop()
